a4a83da4623d1f52a2e10fb27789cf6f0788550d
Nicolai Weitkemper <nico.weio@gmail.com>
2019-11-04 16:28:20 +0100

    Aufgabe 3_1 hinzugefügt

---
+++ Blatt 3/Aufgabe_3_1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  double euro, pound, usd, cny;
+
+  cout << "€: ";
+  cin >> euro;
+
+  std::cout.setf(std::ios::fixed, std::ios::floatfield);
+  std::cout.precision(2);
+
+  pound = euro / 0.86;
+  usd = euro / 1.11;
+  cny = euro / 7.84;
+
+  cout << endl;
+  cout << pound << " Pfund" << endl;
+  cout << usd << " US-Dollar" << endl;
+  cout << cny << " Chinesische Renminbi" << endl;
+}
