a2feb36deb961628527bb21cd2fa41d42159b88d
Nicolai Weitkemper <nico.weio@gmail.com>
2019-11-08 22:57:33 +0100

    Umrechnung korrigiert

--- Blatt 3/Aufgabe_3_1.cpp
+++ Blatt 3/Aufgabe_3_1.cpp
@@ -11,8 +11,8 @@ int main()
   std::cout.precision(2);
 
   pound = euro / 0.86;
-  usd = euro / 1.11;
-  cny = euro / 7.84;
+  usd = euro * 1.11;
+  cny = euro * 7.84;
 
   cout << endl;
   cout << pound << " Pfund" << endl;
