948831137c2320ea4078f8e746ef9503697db5fc
Nicolai Weitkemper <nico.weio@gmail.com>
2019-11-23 12:31:19 +0100

    Aufgabe 6_2 hinzugefügt

---
+++ Blatt 6/Aufgabe_6_2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int ack(int n, int m) {
+  if (n == 0) {
+    return m + 1;
+  } else if (m == 0) {
+    return ack(n - 1, 1);
+  } else {
+    return ack(n - 1, ack(n, m - 1));
+  }
+}
+
+int ack2(int n, int m) {
+  while (n != 0) {
+    if (m == 0) {
+      m = 1;
+    } else {
+      m = ack2(n, m - 1);
+    }
+    n = n - 1;
+  }
+  return m + 1;
+}
+
+int main() {
+  int n = 0, m = 0;
+  cout << "n: ";
+  cin >> n;
+  cout << "m: ";
+  cin >> m;
+
+  cout << "ack: " << ack(n, m) << ", ack2: " << ack2(n, m) << endl;
+}
